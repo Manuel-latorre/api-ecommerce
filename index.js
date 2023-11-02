9160e176fe0748929c40f439a73d6072f5aaa20d
Manuel-latorre <manuel.latorre11@gmail.com>
2023-11-02 13:57:50 -0300

    add-mp

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const express = require('express')
 const cors = require("cors");
+const mercadopago = require('mercadopago')
 
 require("dotenv").config();
 
@@ -34,7 +35,44 @@ app.delete('/products-cart/:productId', controllers.deleteProduct);
 
 
 
+/* MERCADO PAGO */
 
+mercadopago.configure({
+    access_token: process.env.ACCESS_TOKEN,
+})
+
+app.get("/", function(req, res){
+    res.send("backend maercadopago")
+})
+
+app.post("/create_preference", (req, res) => {
+    let preference = {
+        items: [
+            {
+                title: req.body.description,
+                unit_price: Number(req.body.price),
+                quantity: Number(req.body.quantity),
+            },
+        ],
+        back_urls: {
+            success: "http://localhost:8080",
+            failure: "http://localhost:8080",
+            pending: "http://localhost:8080",
+        },
+        auto_return: 'approved',
+    };
+
+    mercadopago.preferences
+    .create(preference)
+    .then((response) => {
+        res.json({ id: response.body.id });
+    })
+    .catch(function (error){
+        console.log(error);
+    })
+
+    console.log(res);
+})
 
 
 
